4e7f3432096ff227ea8d00f488ae2afd08e5768b
sschroeder256 <stephen.schroeder256@gmail.com>
2017-01-21 17:19:52 -0500

    + Added mines

--- Assets/Scripts/SheepBehavior.cs
+++ Assets/Scripts/SheepBehavior.cs
@@ -14,12 +14,22 @@ public class SheepBehavior : MonoBehaviour {
         boxCollider = GetComponent<BoxCollider2D>();
 	}
 
+    public void Explode()
+    {
+        Destroy(this.gameObject);
+    }
+
+    public void FallToDeath()
+    {
+        Destroy(this.gameObject);
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         //if it's a pit, kill it
         if (collision.gameObject.tag == "Terrain")
         {
-            Destroy(this.gameObject);
+            FallToDeath();
         }
 
     }
@@ -33,6 +43,12 @@ public class SheepBehavior : MonoBehaviour {
             Vector3 pushDirection = transform.position - wave.transform.position;
             transform.position = wave.transform.position + pushDirection.normalized * (float)(wave.circleCollider.radius);
             direction = Vector3.Lerp(direction, pushDirection, Time.deltaTime/2);
+        }
+
+        Mine mine = other.gameObject.GetComponent<Mine>();
+        if(mine != null)
+        {
+            mine.Exploding = true;
         }
     }
 
