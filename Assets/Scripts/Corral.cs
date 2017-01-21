33bc312a17caee8e4d3257cc2a969daf164454d0
sschroeder256 <stephen.schroeder256@gmail.com>
2017-01-21 15:05:50 -0500

    + Added a corral object

---
+++ Assets/Scripts/Corral.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Corral : MonoBehaviour {
+
+    public int SheepCount = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.GetComponent<SheepBehavior>() != null)
+        {
+            Destroy(collision.gameObject);
+            SheepCount++;
+        }
+    }
+}
