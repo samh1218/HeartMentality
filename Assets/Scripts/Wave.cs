baae269400363a539288cde7d3f86f761dc2de41
sschroeder256 <stephen.schroeder256@gmail.com>
2017-01-21 13:58:27 -0500

    + Added self-destructing wave objects

---
+++ Assets/Scripts/Wave.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CircleCollider2D))]
+public class Wave : MonoBehaviour {
+
+    private CircleCollider2D circleCollider;
+    private ParticleSystem pSystem;
+
+    public int expandSpeed = 80;
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(transform.position, circleCollider.radius);
+    }
+
+    // Use this for initialization
+    void Start () {
+        circleCollider = GetComponent<CircleCollider2D>();
+        pSystem = GetComponent<ParticleSystem>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        circleCollider.radius += Time.deltaTime * expandSpeed;
+
+        if(pSystem)
+        {
+            if (!pSystem.IsAlive())
+            {
+                Destroy(gameObject);
+            }
+        }
+	}
+}
