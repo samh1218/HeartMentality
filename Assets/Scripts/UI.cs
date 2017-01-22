105605c69162f53844df93a58d91a332207c8593
Michael Stokes <trekbikerfx@gmail.com>
2017-01-22 14:05:03 -0500

    New UI scripts

---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour {
+
+    public Text sheepSavedCounterDisplay;
+    public static int sheepSavedCounter;
+    public Text sheepLostCounterDisplay;
+    public static int sheepLostCounter;
+
+	// Use this for initialization
+	void Start () {
+        sheepSavedCounterDisplay = GetComponent<Text>();
+        sheepLostCounterDisplay = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        sheepSavedCounterDisplay.text = sheepSavedCounter.ToString();
+        sheepLostCounterDisplay.text = sheepLostCounter.ToString();
+	}
+}
